e1b4739b292160de90526665fa662d3d9775f6c2
Gogs <mufrizalrizki@gmail.com>
2018-04-19 16:53:25 +0700

    add wso2 cluster

---
+++ wso2-cluster/copy1.sh
@@ -0,0 +1,6 @@
+cp postgresql-42.2.2.jar /wso2am/repository/components/lib/
+cp gateway1/repository/conf/api-manager.xml /wso2am/repository/conf/
+cp gateway1/repository/conf/carbon.xml /wso2am/repository/conf/
+cp gateway1/repository/conf/log4j.properties /wso2am/repository/conf/
+cp gateway1/repository/conf/axis2/axis2.xml /wso2am/repository/conf/axis2/
+cp gateway1/repository/conf/datasources/*.xml /wso2am/repository/conf/datasources
